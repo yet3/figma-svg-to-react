d6f2489c4f573b08513db6ce0d39d6cd59ef3752
Max Kasperowicz <kasperowicz.maksymilian@gmail.com>
2022-11-21 18:21:05 +0100

    feat: add option to remove fill/stroke and adding classNames based on figma names #1

--- serverless/src/template.ts
+++ serverless/src/template.ts
@@ -3,6 +3,7 @@ import { Template } from '@svgr/babel-plugin-transform-svg-component';
 const SvgrTemplate: Template = (variables, { tpl }) => {
   return tpl`
     ${variables.imports};
+
     ${variables.interfaces};
 
     const ${variables.componentName} = ([args]) => (
--- serverless/src/transformSvg.ts
+++ serverless/src/transformSvg.ts
@@ -3,17 +3,75 @@ import { ISvgNode } from '../../serverless/schemas/svg.schema';
 
 import { transform } from '@svgr/core';
 import { SvgrTemplate } from './template';
+
 import '@svgr/plugin-svgo';
 import '@svgr/plugin-jsx';
 import '@svgr/plugin-prettier';
 
+// @ts-ignore
+
+const escapeClassName = (className: string) => {
+  return className;
+};
+
+function addClassNamesBasedOnIds(root: any, onlyRemoveFirstGroup = false) {
+  let baseClassName = '';
+  const rootEl = root.children[0];
+  if (rootEl.isElem('svg')) {
+    if (rootEl.children.length === 1) {
+      const el = rootEl.children[0];
+      if (el.hasAttr('id') && el.isElem('g')) {
+        baseClassName = escapeClassName(el.attributes.id);
+        if (!onlyRemoveFirstGroup) rootEl.attributes.className = baseClassName;
+        rootEl.children = el.children;
+      }
+    }
+  }
+  if (onlyRemoveFirstGroup) return root;
+
+  const goThroughChildren = (node: any, accCN = '') => {
+    if (node.hasAttr('id')) {
+      accCN += '__' + escapeClassName(node.attributes.id);
+      node.attributes.className = accCN;
+      delete node.attributes.id;
+    }
+
+    if (node.children && node.children.length > 0) {
+      node.children.forEach((child: any) => {
+        goThroughChildren(child, accCN);
+      });
+    }
+  };
+
+  goThroughChildren(rootEl, baseClassName);
+
+  return root;
+}
+
 const transformSvg = async (svg: ISvgNode, options: IOptions): Promise<string | null> => {
-  const { includeImportReact, asIcon, forReactNative, namedExport, withProps, withViewbox, typescript, forwardRef, propsInterface } =
-    options;
+  const {
+    removeAllFill,
+    removeAllStroke,
+    includeImportReact,
+    asIcon,
+    forReactNative,
+    namedExport,
+    withProps,
+    withViewbox,
+    typescript,
+    forwardRef,
+    propsInterface,
+    addClassNamesBasedOnElementsNames,
+  } = options;
 
   let data = '';
   try {
     const code = String.fromCharCode(...svg.bytes);
+    const attrsToRemove: string[] = [];
+    if (removeAllFill) attrsToRemove.push('fill');
+    if (removeAllStroke) attrsToRemove.push('stroke');
+    if (!addClassNamesBasedOnElementsNames) attrsToRemove.push('id');
+
     data = await transform(
       code,
       {
@@ -26,6 +84,7 @@ const transformSvg = async (svg: ISvgNode, options: IOptions): Promise<string | 
         expandProps: withProps ? 'end' : false,
         ref: forwardRef,
         prettier: true,
+        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx', '@svgr/plugin-prettier'],
         svgo: true,
         svgoConfig: {
           plugins: [
@@ -33,10 +92,21 @@ const transformSvg = async (svg: ISvgNode, options: IOptions): Promise<string | 
               name: 'removeViewBox',
               active: !withViewbox,
             },
+            {
+              name: 'addClassNamesBasedOnIds',
+              type: 'full',
+              fn: (root: any) => addClassNamesBasedOnIds(root, !addClassNamesBasedOnElementsNames),
+            },
+            {
+              name: 'removeAttrs',
+              active: attrsToRemove.length > 0,
+              params: {
+                attrs: `(${attrsToRemove.join('|')})`,
+              },
+            },
           ],
         },
         template: SvgrTemplate,
-        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx', '@svgr/plugin-prettier'],
       },
       { componentName: 'COMP_NAME' }
     );
@@ -51,7 +121,7 @@ const transformSvg = async (svg: ISvgNode, options: IOptions): Promise<string | 
   if (withProps) {
     if (typescript) {
       if (propsInterface) {
-        interfacesStr = `\ninterface Props extends SVGProps<SVGSVGElement> {\n\n}\n`;
+        interfacesStr = `interface Props extends SVGProps<SVGSVGElement> {\n\n}`;
       }
       data = data.replace('[props]', 'props[propsType]');
     } else data = data.replace('[props]', 'props');
@@ -84,17 +154,22 @@ const transformSvg = async (svg: ISvgNode, options: IOptions): Promise<string | 
   }
 
   const split = data.split('\n');
-  if (interfacesStr) {
-    for (let i = 0; i < split.length; i++) {
-      const line = split[i];
-      if (line === '') {
-        split[i] = interfacesStr;
-        break;
-      } else if (i === 0 && line.includes('const')) {
-        data = interfacesStr + data;
-        break;
+  let constsI = 0;
+  for (let i = 0; i < split.length; i++) {
+    const line = split[i];
+    if (constsI === 0 && line.includes('const')) {
+      constsI++;
+      split.splice(i, 0, '');
+
+      if (interfacesStr) {
+        split.splice(i, 0, '\n' +interfacesStr);
       }
     }
+
+    if (line === ');') {
+      split.splice(i+1, 0, '');
+    }
+    
   }
 
   return split.join('\n');
