62a1dda012fdaa8f692b534d77f2c547667635b3
Max Kasperowicz <yet3.dev@gmail.com>
2024-10-26 22:32:47 +0200

    test: add tests for "Smart classes"

---
+++ apps/ui/tests/genOptions/smartClasses.test.ts
@@ -0,0 +1,126 @@
+import { REACT_FRAMEWORKS } from "@shared/lib";
+import { FrameworkEnum, type IComponent } from "@shared/types";
+import { parseClassAttr } from "src/lib/svgo/parseClassAttr";
+import type { ElementNode } from "svg-parser";
+import { getTestClassAttr } from "tests/lib/getTestClassAttr";
+import { describe, expect, test } from "vitest";
+import {
+	TEST_SVGS,
+	TEST_SVG_GROUP_ID,
+	TEST_SVG_GROUP_ID_PARSED,
+	TEST_SVG_RECT_1_ID,
+	TEST_SVG_RECT_1_ID_PARSED,
+	TEST_SVG_RECT_2_ID,
+	TEST_SVG_RECT_2_ID_PARSED,
+} from "../lib/consts";
+import { prepareCompTestCtx } from "../lib/prepareCompTestCtx";
+
+describe("smart classes", () => {
+	test("parse class attribute", () => {
+		expect(parseClassAttr("Group 1")).toBe("Group1");
+		expect(parseClassAttr("Group_1")).toBe("Group_1");
+		expect(parseClassAttr("21Group_1")).toBe("Group_1");
+		expect(parseClassAttr("21Group:1")).toBe("Group\\:1");
+		expect(parseClassAttr("Group 1 (special)")).toBe("Group1\\(special\\)");
+		expect(parseClassAttr("Group_1__Rectangle_1")).toBe("Group_1__Rectangle_1");
+
+		expect(parseClassAttr(TEST_SVG_GROUP_ID)).toBe(TEST_SVG_GROUP_ID_PARSED);
+		expect(parseClassAttr(TEST_SVG_RECT_1_ID)).toBe(TEST_SVG_RECT_1_ID_PARSED);
+		expect(parseClassAttr(TEST_SVG_RECT_2_ID)).toBe(TEST_SVG_RECT_2_ID_PARSED);
+	});
+
+	const checkCodeForClasses = (
+		component: IComponent,
+		framework: FrameworkEnum,
+	) => {
+		const attrName = REACT_FRAMEWORKS.includes(framework)
+			? "className"
+			: "class";
+
+		return component.code.match(new RegExp(`${attrName}=`, "gm"));
+	};
+
+	for (const framework of Object.values(FrameworkEnum)) {
+		describe(framework, () => {
+			describe("Group with 2 rects", () => {
+				test("should have BEM classes on <svg> and 2 <rects>", async () => {
+					const { svgEl, component } = await prepareCompTestCtx({
+						svgCode: TEST_SVGS.GROUP_2_RECTS_WITH_IDS,
+						framework,
+						genOptions: {
+							smartClasses: true,
+						},
+					});
+
+					expect(checkCodeForClasses(component, framework)).toHaveLength(3);
+					expect(getTestClassAttr(svgEl, framework)).toBe(
+						TEST_SVG_GROUP_ID_PARSED,
+					);
+
+					expect(
+						getTestClassAttr(svgEl.children[0] as ElementNode, framework),
+					).toBe(`${TEST_SVG_GROUP_ID_PARSED}__${TEST_SVG_RECT_1_ID_PARSED}`);
+					expect(
+						getTestClassAttr(svgEl.children[1] as ElementNode, framework),
+					).toBe(`${TEST_SVG_GROUP_ID_PARSED}__${TEST_SVG_RECT_2_ID_PARSED}`);
+				});
+
+				test("should have exact classes on <svg> and 2 <rects>", async () => {
+					const { svgEl, component } = await prepareCompTestCtx({
+						svgCode: TEST_SVGS.GROUP_2_RECTS_WITH_IDS,
+						framework,
+						genOptions: {
+							smartClasses: true,
+							smartClassesExact: true,
+						},
+					});
+
+					expect(checkCodeForClasses(component, framework)).toHaveLength(3);
+					expect(getTestClassAttr(svgEl, framework)).toBe(
+						TEST_SVG_GROUP_ID_PARSED,
+					);
+
+					expect(
+						getTestClassAttr(svgEl.children[0] as ElementNode, framework),
+					).toBe(TEST_SVG_RECT_1_ID_PARSED);
+					expect(
+						getTestClassAttr(svgEl.children[1] as ElementNode, framework),
+					).toBe(TEST_SVG_RECT_2_ID_PARSED);
+				});
+
+				test("should have class only on <svg>", async () => {
+					const { svgEl, component } = await prepareCompTestCtx({
+						svgCode: TEST_SVGS.GROUP_2_RECTS_WITH_IDS,
+						framework,
+						genOptions: {
+							smartClasses: true,
+							smartClassesOnlySvg: true,
+						},
+					});
+
+					expect(checkCodeForClasses(component, framework)).toHaveLength(1);
+					expect(getTestClassAttr(svgEl, framework)).toBe(
+						TEST_SVG_GROUP_ID_PARSED,
+					);
+				});
+			});
+
+			describe("One rect", () => {
+				test("should have class on <svg>", async () => {
+					const { svgEl, component } = await prepareCompTestCtx({
+						svgCode: TEST_SVGS.RECTS_WITH_ID,
+						framework,
+						genOptions: {
+							smartClasses: true,
+						},
+					});
+
+					expect(checkCodeForClasses(component, framework)).toHaveLength(1);
+					expect(getTestClassAttr(svgEl, framework)).toBe(
+						TEST_SVG_RECT_1_ID_PARSED,
+					);
+				});
+			});
+		});
+	}
+});
--- apps/ui/tests/lib/consts.ts
+++ apps/ui/tests/lib/consts.ts
@@ -5,12 +5,22 @@ export const TEST_SVG_WIDTH = 144;
 export const TEST_SVG_HEIGHT = 144;
 export const TEST_SVG_VIEWBOX = "0 0 144 144";
 
+export const TEST_SVG_GROUP_ID = "Group 1";
+export const TEST_SVG_GROUP_ID_PARSED = "Group1";
+export const TEST_SVG_RECT_1_ID = "1Rectangle_14";
+export const TEST_SVG_RECT_1_ID_PARSED = "Rectangle_14";
+export const TEST_SVG_RECT_2_ID = "Rectangle-17";
+export const TEST_SVG_RECT_2_ID_PARSED = "Rectangle-17";
+
 export const TEST_SVGS = {
 	SIMPLE_RECT: `<svg width="${TEST_SVG_WIDTH}" height="${TEST_SVG_HEIGHT}" viewBox="${TEST_SVG_VIEWBOX}" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="144" height="144" fill="#FF00F2" stroke="red" /></svg>`,
 	SIMPLE_RECT_OPTIMIZED: `<svg xmlns="http://www.w3.org/2000/svg" width="${TEST_SVG_WIDTH}" height="${TEST_SVG_HEIGHT}" fill="none" viewBox="${TEST_SVG_VIEWBOX}" SPP_SPREAD_PROPS="" SPP_REF=""><path fill="#FF00F2" stroke="red" d="M0 0h144v144H0z"/></svg>`,
 
 	SIMPLE_RECT_WITH_TITLE: `<svg width="${TEST_SVG_WIDTH}" height="${TEST_SVG_HEIGHT}" viewBox="${TEST_SVG_VIEWBOX}" fill="none" xmlns="http://www.w3.org/2000/svg"><title>${TEST_SVG_TITLE}</title><rect width="144" height="144" fill="#FF00F2"/></svg>`,
 	SIMPLE_RECT_EMPTY_TITLE: `<svg width="${TEST_SVG_WIDTH}" height="${TEST_SVG_HEIGHT}" viewBox="${TEST_SVG_VIEWBOX}" fill="none" xmlns="http://www.w3.org/2000/svg"><title></title><rect width="144" height="144" fill="#FF00F2"/></svg>`,
+
+	GROUP_2_RECTS_WITH_IDS: `<svg width="${TEST_SVG_WIDTH}" height="${TEST_SVG_HEIGHT}" viewBox="${TEST_SVG_VIEWBOX}" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="${TEST_SVG_GROUP_ID}"><rect id="${TEST_SVG_RECT_1_ID}" width="144" height="144" fill="#FF00F2"/><rect id="${TEST_SVG_RECT_2_ID}" width="144" height="144" fill="#00FFF2"/></g></svg>`,
+	RECTS_WITH_ID: `<svg width="${TEST_SVG_WIDTH}" height="${TEST_SVG_HEIGHT}" viewBox="${TEST_SVG_VIEWBOX}" fill="none" xmlns="http://www.w3.org/2000/svg"><rect id="${TEST_SVG_RECT_1_ID}" width="144" height="144" fill="#FF00F2"/></svg>`,
 };
 
 export const OG_SVG_ID = "og-svg";
